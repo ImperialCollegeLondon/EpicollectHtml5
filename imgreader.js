b7e8b4affe2c309eb9d3c6f2ba1fbd2a53576219
Ray Cheung <zhangpin04@gmail.com>
2011-03-24 17:23:46 +0800

    1. save image to locale storage and show them, but local storage has limited size, we should use indexedDB instead.
    Signed-off-by: Ray Cheung <zhangpin04@gmail.com>

--- imgreader.js
+++ imgreader.js
@@ -1,3 +1,19 @@
+function init() {
+	if (null == window.localStorage.getItem('items')) {
+		window.localStorage.setItem('items', JSON.stringify([]));
+	} else {
+		document.getElementById('gallary').innerHTML = '';
+		var items = JSON.parse(window.localStorage.getItem('items'));
+		for (var i = 0; i < items.length; i++) {
+			onAddNewItem(items[i]);
+		}
+	}
+}
+
+window.addEventListener('load', function(even) {
+	init();
+}, false);
+
 function onchange_file(input) {
 	var file = input.files[0];
 	
@@ -11,7 +27,6 @@ function onchange_file(input) {
 	reader.onload = (function(aImg) {
 		return function(e) {
 			aImg.src = e.target.result;
-			document.getElementById('file-info').textContent  = aImg.src;
 		};
 	})(document.getElementById('img-previewer'));
 	
@@ -20,4 +35,31 @@ function onchange_file(input) {
 	};
 	
 	reader.readAsDataURL(file);
+}
+
+function onClickSaveToLocal(input) {
+	var dataUrl = document.getElementById('img-previewer').src;
+	if (!dataUrl) {
+		return;
+	}
+	
+	var obj = {};
+	obj['img'] = document.getElementById('img-previewer').src;
+	
+	var items = JSON.parse(window.localStorage.getItem('items'));
+	var itemId = 'item-' + new Date().getTime();
+	items.push(itemId);
+	
+	window.localStorage.setItem('items', JSON.stringify(items));
+	window.localStorage.setItem(itemId, JSON.stringify(obj));
+	
+	onAddNewItem(itemId);
+}
+
+function onAddNewItem(itemId) {
+	var item = JSON.parse(window.localStorage.getItem(itemId));
+	var img = document.createElement('IMG');
+	img.className = 'preview-image';
+	img.src = item['img'];
+	document.getElementById('gallary').appendChild(img);
 }
